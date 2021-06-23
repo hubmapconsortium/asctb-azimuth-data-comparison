3326fd4aca0800b0c9a870b80f5785176a207aba
Darshal Shetty <darshalcs@gmail.com>
2021-06-23 01:17:23 -0400

    Remove adding first column that contained organ name

--- extract_ct_from_ref.R
+++ extract_ct_from_ref.R
@@ -10,8 +10,7 @@ ref <- readRDS(stringr::str_interp("azimuth_references/${body_organ}.Rds"))
 cell_types <- unique(ref@meta.data[cell_hierarchy_cols])
 # sorting not necessary
 # cell_types <- cell_types[order(cell_types$annotation.l1, cell_types$annotation.l2),]
-names(cell_types) <- sapply(1:length(cell_hierarchy_cols), function (x) return(stringr::str_interp("AS/${x+1}")))
-cell_types$`AS/1` <- as.factor(rep(body_organ , nrow(cell_types)))
+names(cell_types) <- sapply(1:length(cell_hierarchy_cols), function (x) return(stringr::str_interp("AS/${x}")))
 
 empty_rows <- matrix(rep(NA, length(cell_hierarchy_cols)*10), 10,length(cell_hierarchy_cols))
 write.table(empty_rows, 
@@ -20,8 +19,7 @@ write.table(empty_rows,
             na = "",
             row.names = FALSE, 
             col.names = FALSE)
-write.table(cell_types[,c(length(cell_hierarchy_cols)+1,
-                          1:length(cell_hierarchy_cols))], # reorder columns
+write.table(cell_types,
             file = stringr::str_interp("asctb_tables/${body_organ}.csv"),
             sep = ',',
             append = TRUE,
