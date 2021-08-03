35b7b9048ec8723c655d22c1de6330e88d6c86ea
Darshal Shetty <darshalcs@gmail.com>
2021-08-03 18:28:07 -0400

    Make deployed website display ASCTB names for CSV files

--- bash/build.sh
+++ bash/build.sh
@@ -4,5 +4,5 @@ mkdir dist
 cp data/asctb_tables/*.csv dist
 cp public/* dist
 mv dist/index-template.md dist/index.md
-jq -r '.references[].name' data/organ_data.json |\
-  awk 'BEGIN{OFS=""} {print "- [",$1,"](",$1,".csv)"}' >> dist/index.md
+jq -r '.references[]|"\(.name)|\(.asctb_name)"' data/organ_data.json |\
+  awk -F "|" 'BEGIN{OFS=""} {print "- [",$2,"](",$1,".csv)"}' >> dist/index.md
