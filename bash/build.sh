207f874c74bb13823e8b0bedc5ab48ec2192bc49
Vikrant Deshpande <32123572+vikrantdeshpande09876@users.noreply.github.com>
2021-09-28 12:55:55 -0400

    Updated build.sh
    Cleaning the code

--- bash/build.sh
+++ bash/build.sh
@@ -7,7 +7,6 @@ cp public/* dist
 mv dist/index-template.md dist/index.md
 # Parse the references[] json from 'organ_data.json' and print each "- organ_name: URL for file_name.csv". Currently hardcoded the string for spleen since we don't have Cell-type info in the custom JSON file on S3.
 jq -r '.references[]|"\(.name)|\(.asctb_name)"' data/organ_data.json |\
-  jq -r '.references[]|"\(.name)|\(.asctb_name)"' data/organ_data.json |\
   awk -F "|" 'BEGIN{OFS=""} {print ($1 ~ /spleen/ ? "- ["$2"](https://hubmapconsortium.github.io/asctb-azimuth-data-comparison/"$1".csv)" : "- ["$2"](https://hubmapconsortium.github.io/asctb-azimuth-data-comparison/"$1".csv) : [Summary of Cell-Types](https://hubmapconsortium.github.io/asctb-azimuth-data-comparison/"$1".celltype_stats.csv)")}'|\
    sort > dist/csv_download_list.md
 
